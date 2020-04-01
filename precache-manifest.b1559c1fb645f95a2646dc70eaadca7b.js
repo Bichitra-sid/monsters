b70781623fa259bb213da67cfa2c28048bdb144f
Bichitra-sid <bichitra.sid@gmail.com>
2020-04-01 15:47:18 +0530

    Updates

---
+++ precache-manifest.b1559c1fb645f95a2646dc70eaadca7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e8534f07a45d956795d4c6f12cd41a",
+    "url": "/Bichitra-sid/React-Projects/tree/master/monster/index.html"
+  },
+  {
+    "revision": "1cc71efd84f444a71e32",
+    "url": "/Bichitra-sid/React-Projects/tree/master/monster/static/css/main.041af2db.chunk.css"
+  },
+  {
+    "revision": "2908fc9c87205179f510",
+    "url": "/Bichitra-sid/React-Projects/tree/master/monster/static/js/2.b01c30b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Bichitra-sid/React-Projects/tree/master/monster/static/js/2.b01c30b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cc71efd84f444a71e32",
+    "url": "/Bichitra-sid/React-Projects/tree/master/monster/static/js/main.3eceac70.chunk.js"
+  },
+  {
+    "revision": "304e99f077550d21b523",
+    "url": "/Bichitra-sid/React-Projects/tree/master/monster/static/js/runtime-main.15e40d8a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Bichitra-sid/React-Projects/tree/master/monster/static/media/logo.5d5d9eef.svg"
+  }
+]);
